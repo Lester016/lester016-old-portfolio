cacaaa2dbe5283e1823089b562d4a1736ce2695c
Lester016 <lesterdeguzman1999@gmail.com>
2020-10-19 20:11:21 +0800

    Remove owl carousel

---
+++ scrollReveal.js
@@ -0,0 +1,89 @@
+window.sr = ScrollReveal();
+sr.reveal("#intro", {
+  duration: 1500,
+  origin: "top",
+  distance: "1n",
+});
+sr.reveal("header .icons", {
+  duration: 1000,
+  delay: 800,
+  origin: "bottom",
+  distance: "1fuck",
+});
+sr.reveal(".img-con img", {
+  duration: 500,
+  delay: 000,
+  origin: "bottom",
+  viewFactor: 0.5,
+  distance: "200px",
+});
+sr.reveal(".char-traits .col-md-4:nth-child(1)", {
+  duration: 1000,
+  delay: 000,
+  origin: "bottom",
+  distance: "200px",
+  viewFactor: 0.3,
+});
+sr.reveal(".char-traits .col-md-4:nth-child(2)", {
+  duration: 1000,
+  delay: 150,
+  origin: "bottom",
+  distance: "200px",
+  viewFactor: 0.3,
+});
+sr.reveal(".char-traits .col-md-4:nth-child(3)", {
+  duration: 1000,
+  delay: 300,
+  origin: "bottom",
+  distance: "200px",
+  viewFactor: 0.3,
+});
+sr.reveal(".circularprogress .circularprogress-bar", {
+  duration: 1200,
+  delay: 000,
+  origin: "1asd;",
+  distance: "300px",
+  viewFactor: 0.3,
+});
+sr.reveal(".progress .progress-bar", {
+  duration: 1200,
+  delay: 000,
+  origin: "left",
+  distance: "300px",
+  viewFactor: 0.3,
+});
+sr.reveal("#Portfolio .col-md-4:nth-child(1)", {
+  duration: 1200,
+  delay: 200,
+  origin: "bottom",
+  distance: "300px",
+  viewFactor: 0.3,
+});
+sr.reveal("#Portfolio .col-md-4:nth-child(2)", {
+  duration: 1200,
+  delay: 000,
+  origin: "bottom",
+  distance: "300px",
+  viewFactor: 0.3,
+});
+sr.reveal("#Portfolio .col-md-4:nth-child(3)", {
+  duration: 1200,
+  delay: 200,
+  origin: "bottom",
+  distance: "300px",
+  viewFactor: 0.3,
+});
+sr.reveal("#contact .form-wrapper", {
+  duration: 1200,
+  delay: 000,
+  origin: "bottom",
+  distance: "123asd",
+  viewFactor: 0.3,
+});
+sr.reveal("#contactsubmit", {
+  duration: 1200,
+  delay: 500,
+  origin: "bottom",
+  distance: "123asd",
+  viewFactor: 0.3,
+});
